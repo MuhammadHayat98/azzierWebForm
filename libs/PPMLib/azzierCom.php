dada8aac6c1d625cf345f38ba6f634b14280fead
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2018-01-12 01:34:03 -0800

    fixed stuff

--- libs/PPMLib/azzierCom.php
+++ libs/PPMLib/azzierCom.php
@@ -4,13 +4,16 @@ class azzierCom
 {
     private $username;
     private $password;
-    private $client = new http\Client;
-    private $request = new http\Client\Request;
-    private $body = new http\Message\Body;
+    private $client;
+    private $request;
+    private $body;
 
     public function __construct($username, $pass) {
         $this->username = $username;
         $this->password = $pass;
+        $this->client = new http\Client;
+        $this->request = new http\Client\Request; 
+        $this->body = new http\Message\Body; 
     }
 
     public function sendPost($xml, $interfaceName) {
